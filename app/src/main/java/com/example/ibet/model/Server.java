d95a1eb578225b9868f8e34b5576356d4d6aac4c
Eden Shtein <edenshtein30@cs.colman.ac.il>
2021-02-21 20:54:18 +0200

    Added post reqeust

--- app/src/main/java/com/example/ibet/model/Server.java
+++ app/src/main/java/com/example/ibet/model/Server.java
@@ -4,14 +4,71 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 
 public class Server {
 
     public void signUp(String email,String password)
     {
+
+        // Create data variable for sent values to server
+        String data = null;
+        try {
+            data = URLEncoder.encode("email", "UTF-8")
+                    + "=" + URLEncoder.encode(email, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        try {
+            data += "&" + URLEncoder.encode("password", "UTF-8") + "="
+                    + URLEncoder.encode(password, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        String text = "";
+        BufferedReader reader=null;
+        // Send data
+        try
+        {
+            // Defined URL  where to send data
+            URL url = new URL("http://localhost:3000");
+            // Send POST data request
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write( data );
+            wr.flush();
+            // Get the server response
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            // Read Server Response
+            while((line = reader.readLine()) != null)
+            {
+                // Append server response in string
+                sb.append(line + "\n");
+            }
+            text = sb.toString();
+        }
+        catch(Exception ex)
+        {
+
+        }
+        finally
+        {
+            try
+            {
+
+                reader.close();
+            }
+
+            catch(Exception ex) {}
+        }
 
     }
 }

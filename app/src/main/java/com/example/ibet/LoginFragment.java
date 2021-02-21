ae9eecdbf10708f58a7fd099839f1e8d5d41d239
Or Levi <levior9@gmail.com>
2021-02-21 13:10:57 +0200

    init styling

--- app/src/main/java/com/example/ibet/LoginFragment.java
+++ app/src/main/java/com/example/ibet/LoginFragment.java
@@ -3,18 +3,32 @@ package com.example.ibet;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class LoginFragment extends Fragment {
+
+    Button signup;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        signup = view.findViewById(R.id.signin_signup_btn);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_login_to_signUp);
+            }
+        });
+
+        return view;
     }
 }

ab1298d18fd3f91f0c8728dd805b38ddfaa3cd7a
Eden Shtein <edenshtein30@cs.colman.ac.il>
2021-02-21 21:48:09 +0200

    Server

--- app/src/main/java/com/example/ibet/model/Server.java
+++ app/src/main/java/com/example/ibet/model/Server.java
@@ -1,10 +1,16 @@
 package com.example.ibet.model;
 
+import android.util.Log;
+
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -15,60 +21,43 @@ public class Server {
     public void signUp(String email,String password)
     {
 
-        // Create data variable for sent values to server
-        String data = null;
-        try {
-            data = URLEncoder.encode("email", "UTF-8")
-                    + "=" + URLEncoder.encode(email, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        try {
-            data += "&" + URLEncoder.encode("password", "UTF-8") + "="
-                    + URLEncoder.encode(password, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://localhost:3000/api/users/signup");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                    conn.setRequestProperty("Accept","application/json");
+                    conn.setDoOutput(true);
+                    conn.setDoInput(true);
 
-        String text = "";
-        BufferedReader reader=null;
-        // Send data
-        try
-        {
-            // Defined URL  where to send data
-            URL url = new URL("http://localhost:3000");
-            // Send POST data request
-            URLConnection conn = url.openConnection();
-            conn.setDoOutput(true);
-            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-            wr.write( data );
-            wr.flush();
-            // Get the server response
-            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-            // Read Server Response
-            while((line = reader.readLine()) != null)
-            {
-                // Append server response in string
-                sb.append(line + "\n");
+                    JSONObject jsonParam = new JSONObject();
+                    jsonParam.put("email", email);
+                    jsonParam.put("password", password);
+                    jsonParam.put("passwordConfirm", password);
+
+
+                    Log.i("JSON", jsonParam.toString());
+                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                    //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
+                    os.writeBytes(jsonParam.toString());
+
+                    os.flush();
+                    os.close();
+
+                    Log.i("STATUS", String.valueOf(conn.getResponseCode()));
+                    Log.i("MSG" , conn.getResponseMessage());
+
+                    conn.disconnect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
-            text = sb.toString();
-        }
-        catch(Exception ex)
-        {
+        });
 
-        }
-        finally
-        {
-            try
-            {
-
-                reader.close();
-            }
-
-            catch(Exception ex) {}
-        }
+        thread.start();
 
     }
 }

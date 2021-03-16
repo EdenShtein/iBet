82b3fab2bad767d6e441caed46d6ea5c2b67c032
Eden Shtein <edenshtein30@cs.colman.ac.il>
2021-03-16 14:41:57 +0200

    Added progress bar to the teams results fragments and changed the logic of initialization of the table

--- app/src/main/java/com/example/ibet/TeamsResultFragment.java
+++ app/src/main/java/com/example/ibet/TeamsResultFragment.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -27,6 +28,9 @@ public class TeamsResultFragment extends Fragment {
    View view;
 
     Button button;
+    TableLayout table;
+
+    ProgressBar pb;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,85 +39,91 @@ public class TeamsResultFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_teams_result, container, false);
 
 
-        button=view.findViewById(R.id.teamsresults_button);
-        TableLayout table = view.findViewById(R.id.table);
+        //button=view.findViewById(R.id.teamsresults_button);
+        table = view.findViewById(R.id.table);
+        pb=view.findViewById(R.id.teamsresults_pb);
 
 
-        button.setOnClickListener(new View.OnClickListener() {
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+        pb.setVisibility(View.VISIBLE);
+        initTable();
+        return view;
+    }
+
+    public void initTable(){
+        Model.instance.getAlgoResult(new Model.TeamDataListener() {
             @Override
-            public void onClick(View v) {
-                Model.instance.getAlgoResult(new Model.TeamDataListener() {
+            public void onComplete(ArrayList<Team> teamData) {
+
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
                     @Override
-                    public void onComplete(ArrayList<Team> teamData) {
+                    public void run() {
 
-                        new Handler(Looper.getMainLooper()).post(new Runnable() {
-                            @Override
-                            public void run() {
+                        TableRow tbrow0 = new TableRow(getActivity().getApplicationContext());
+                        TextView tr0 = new TextView(getActivity().getApplicationContext());
+                        tr0.setText(" Team Name ");
+                        tr0.setTextColor(Color.WHITE);
+                        tbrow0.addView(tr0);
+                        TextView tr1 = new TextView(getActivity().getApplicationContext());
+                        tr1.setText(" Wins ");
+                        tr1.setTextColor(Color.WHITE);
+                        tbrow0.addView(tr1);
+                        TextView tr2 = new TextView(getActivity().getApplicationContext());
+                        tr2.setText(" Losses ");
+                        tr2.setTextColor(Color.WHITE);
+                        tbrow0.addView(tr2);
+                        TextView tr3 = new TextView(getActivity().getApplicationContext());
+                        tr3.setText(" Eliminated? ");
+                        tr3.setTextColor(Color.WHITE);
+                        tbrow0.addView(tr3);
+                        table.addView(tbrow0);
 
-                                TableRow tbrow0 = new TableRow(getActivity().getApplicationContext());
-                                TextView tr0 = new TextView(getActivity().getApplicationContext());
-                                tr0.setText(" Team Name ");
-                                tr0.setTextColor(Color.WHITE);
-                                tbrow0.addView(tr0);
-                                TextView tr1 = new TextView(getActivity().getApplicationContext());
-                                tr1.setText(" Wins ");
-                                tr1.setTextColor(Color.WHITE);
-                                tbrow0.addView(tr1);
-                                TextView tr2 = new TextView(getActivity().getApplicationContext());
-                                tr2.setText(" Losses ");
-                                tr2.setTextColor(Color.WHITE);
-                                tbrow0.addView(tr2);
-                                TextView tr3 = new TextView(getActivity().getApplicationContext());
-                                tr3.setText(" Eliminated? ");
-                                tr3.setTextColor(Color.WHITE);
-                                tbrow0.addView(tr3);
-                                table.addView(tbrow0);
+                        for(int i=0;i<teamData.size();i++)
+                        {
+                            TableRow row= new TableRow(getActivity().getApplicationContext());
+                            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+                            row.setLayoutParams(lp);
 
-                                for(int i=0;i<teamData.size();i++)
-                                {
-                                    TableRow row= new TableRow(getActivity().getApplicationContext());
-                                    TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
-                                    row.setLayoutParams(lp);
+                            String teamName = teamData.get(i).getTeamName();
+                            TextView name = new TextView(getActivity().getApplicationContext());
+                            name.setText(teamName);
+                            name.setGravity(Gravity.LEFT);
+                            row.addView(name);
 
-                                    String teamName = teamData.get(i).getTeamName();
-                                    TextView name = new TextView(getActivity().getApplicationContext());
-                                    name.setText(teamName);
-                                    name.setGravity(Gravity.CENTER);
-                                    row.addView(name);
+                            String wins = teamData.get(i).getWins();
+                            TextView win = new TextView(getActivity().getApplicationContext());
+                            win.setText(wins);
+                            win.setGravity(Gravity.CENTER);
+                            row.addView(win);
 
-                                    String wins = teamData.get(i).getWins();
-                                    TextView win = new TextView(getActivity().getApplicationContext());
-                                    win.setText(wins);
-                                    win.setGravity(Gravity.CENTER);
-                                    row.addView(win);
+                            String losses = teamData.get(i).getLosses();
+                            TextView lose = new TextView(getActivity().getApplicationContext());
+                            lose.setText(losses);
+                            lose.setGravity(Gravity.CENTER);
+                            row.addView(lose);
 
-                                    String losses = teamData.get(i).getLosses();
-                                    TextView lose = new TextView(getActivity().getApplicationContext());
-                                    lose.setText(losses);
-                                    lose.setGravity(Gravity.CENTER);
-                                    row.addView(lose);
-
-                                    Boolean isEliminated = teamData.get(i).getEliminated();
-                                    TextView eliminated = new TextView(getActivity().getApplicationContext());
-                                    eliminated.setText(String.valueOf(isEliminated));
-                                    eliminated.setGravity(Gravity.CENTER);
-                                    row.addView(eliminated);
+                            Boolean isEliminated = teamData.get(i).getEliminated();
+                            TextView eliminated = new TextView(getActivity().getApplicationContext());
+                            eliminated.setText(String.valueOf(isEliminated));
+                            eliminated.setGravity(Gravity.CENTER);
+                            row.addView(eliminated);
 
 
-                                    table.addView(row,i);
+                            table.addView(row,i);
 
 
-                                }
-                                button.setVisibility(View.INVISIBLE);
-                                button.setEnabled(false);
-                            }
-                        });
+                        }
+                        pb.setVisibility(View.INVISIBLE);
+//                        button.setVisibility(View.INVISIBLE);
+//                        button.setEnabled(false);
                     }
                 });
             }
         });
-
-
-        return view;
     }
 }

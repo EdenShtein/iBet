584436436145682374718f9af7a7621c4fc6a809
Eden Shtein <edenshtein30@cs.colman.ac.il>
2021-03-27 15:01:07 +0300

    minor changes on server.java

--- app/src/main/java/com/example/ibet/model/Server.java
+++ app/src/main/java/com/example/ibet/model/Server.java
@@ -26,7 +26,7 @@ import java.util.ArrayList;
 
 public class Server {
 
-    public String ip = "192.168.1.113";
+    public String ip = "192.168.88.164";
 
     public void signUp(String email, String password, Model.SuccessListener listener, Activity mActivity) {
         RequestQueue requestQueue = Volley.newRequestQueue(mActivity.getApplicationContext());

ab3c39be184cd1ef1f511e95031ef04c3e4180ee
Eden Shtein <edenshtein30@cs.colman.ac.il>
2021-05-08 17:39:15 +0300

    added daily notification

--- app/src/main/java/com/example/ibet/MainActivity.java
+++ app/src/main/java/com/example/ibet/MainActivity.java
@@ -5,11 +5,18 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.SystemClock;
 
 import com.example.ibet.model.Model;
+import com.example.ibet.model.Notifications.AlarmNotificationReceiver;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,9 +39,32 @@ public class MainActivity extends AppCompatActivity {
                 userIsLogin();
             }
         }
+        startAlarm(true,true);
+
     }
 
     public void userIsLogin() {
         navController.navigate(R.id.action_login_to_mainFreed);
+    }
+
+    private void startAlarm(boolean isNotification, boolean isRepeat) {
+        AlarmManager manager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        Intent myIntent;
+        PendingIntent pendingIntent;
+
+        // SET TIME HERE
+        Calendar calendar= Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY,8);
+        calendar.set(Calendar.MINUTE,00);
+
+
+        myIntent = new Intent(MainActivity.this, AlarmNotificationReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(this,0,myIntent,0);
+
+
+        if(!isRepeat)
+            manager.set(AlarmManager.RTC_WAKEUP, SystemClock.elapsedRealtime()+3000,pendingIntent);
+        else
+            manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY,pendingIntent);
     }
 }
